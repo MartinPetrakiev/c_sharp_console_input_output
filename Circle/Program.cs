0decb45f30b2af0db9a188e6b587bf67f51f8aec
MartinPetrakiev <martomsl@gmail.com>
2023-08-09 04:32:34 +0300

    solutions from branch1

---
+++ Circle/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        public static double CalculateArea(double r)
+        {
+            double pi = Math.PI;
+            return pi * Math.Pow(r, 2);
+        }
+
+        public static double CalculatePerimeter(double r)
+        {
+            double pi = Math.PI;
+            return 2 * pi * r;
+        }
+
+        static void Main(string[] args)
+        {
+            double radius;
+            double? Temp_Radius = double.TryParse(Console.ReadLine(), out radius)
+            ? (double?)radius
+            : null;
+
+            Console.WriteLine("{0:F2} {1:F2}", CalculatePerimeter(radius), CalculateArea(radius));
+
+        }
+    }
+}
+
+/*
+ Description
+ Write a program that reads from the console the radius r of a circle and prints its perimeter and area, rounded and formatted with 2 digits after the decimal point.
+
+ Input
+ On the only line of the input you will receive the radius of the circle - r
+
+ Output
+ You should print one line only: the perimeter and the area of the circle, separated by a whitespace, and with 2 digits precision
+
+ Constraints
+ The radius r will always be a valid and positive real number
+ Time limit: 0.1s
+ Memory limit: 16MB
+ */
---
+++ Number_Comparer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            double? Temp_A = double.TryParse(Console.ReadLine(), out a)
+                        ? (double?)a
+                        : null;
+            double? Temp_B = double.TryParse(Console.ReadLine(), out b)
+                        ? (double?)b
+                        : null;
+
+            double result = a >= b ? a : b;
+            Console.WriteLine(result);
+
+        }
+    }
+}
+
+/*
+Description
+Write a program that gets two numbers from the console and prints the greater of them.
+
+Input
+On the first two lines you will receive the two numbers, A and B
+
+Output
+On the only line print the larger of the two numbers
+*Try implementing it without using if-statements
+
+Constraints
+The input will always be valid and in the described format.
+The numbers A and B will always be valid real number
+Time limit: 0.1s
+Memory limit: 16MB
+*/
