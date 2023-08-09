d5540aa6ccf1a6986d817eedadde1b878516ebb2
martinmp <martomsl@gmail.com>
2023-08-09 15:13:06 +0300

    fix first condition output

--- Quadratic_Equation/Program.cs
+++ Quadratic_Equation/Program.cs
@@ -9,11 +9,20 @@ namespace MyProject
             double discriminant = (Math.Pow(b, 2)) - (4 * a * c);
             if (discriminant > 0)
             {
-                double x1 = (0 - b - Math.Sqrt(discriminant)) / 4;
-                double x2 = (0 - b + Math.Sqrt(discriminant)) / 4;
+                double x1 = (0 - b + Math.Sqrt(discriminant)) / (2 * a);
+                double x2 = (0 - b - Math.Sqrt(discriminant)) / (2 * a);
 
-                Console.WriteLine("{0:f2}", x1);
-                Console.WriteLine("{0:f2}", x2);
+                if (x1 < x2)
+                {
+                    Console.WriteLine("{0:f2}", x1);
+                    Console.WriteLine("{0:f2}", x2);
+                }
+                else
+                {
+                    Console.WriteLine("{0:f2}", x2);
+                    Console.WriteLine("{0:f2}", x1);
+                }
+
             }
             else if (discriminant == 0)
             {
