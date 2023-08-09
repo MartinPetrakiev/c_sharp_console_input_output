e96b42e503d7f4569a1c50a4ffe91122611ffac4
martinmp <martomsl@gmail.com>
2023-08-09 15:10:14 +0300

    rest tasks

---
+++ Sum_of_5_numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + b + c + d + e);
+        }
+    }
+}
+
+
+
+/*
+Description
+Write a program that reads 5 integer numbers from the console and prints their sum.
+
+Input
+You will receive 5 numbers on five separate lines.
+
+Output
+Your output should consist only of a single line - the sum of the 5 numbers.
+
+Constraints
+All 5 numbers will always be valid integer numbers between -1000 and 1000, inclusive
+Time limit: 0.1s
+Memory limit: 16MB
+*/
---
+++ Sum_of_N_Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            float sum = 0f;
+
+            for (int i = 1; i <= n; i++)
+            {
+                float nextNumber = float.Parse(Console.ReadLine());
+                sum += nextNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+/*
+Description
+Write a program that enters a number N and after that enters more N numbers and calculates and prints their sum.
+Note: You may need to use a for-loop.
+
+Input
+On the first line, you will receive the number N
+On each of the next N lines, you will receive a floating-point number
+
+Output
+On must output only one number - the sum of the N numbers
+
+Constraints
+1 <= N <= 200
+All numbers will be valid floating-point numbers in the range [-1000, 1000]
+Time limit: 0.1s
+Memory limit: 16MB
+*/
